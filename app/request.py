c9391388c744473cdf90cb38ee0197dd1eb5ca94
Theonilah Owali <theonilahtash@gmail.com>
2018-11-09 16:06:35 +0300

    defined a search source which inturn returns a source results

--- app/request.py
+++ app/request.py
@@ -75,17 +75,17 @@ def get_source(id):
 
     return source_object
 
-def search_movie(movie_name):
-    search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
-    with urllib.request.urlopen(search_movie_url) as url:
-        search_movie_data = url.read()
-        search_movie_response = json.loads(search_movie_data)
+def search_source(source_name):
+    search_source_url = 'https://newsapi.org/v2/sources?api_key={}&query={}'.format(api_key,source_name)
+    with urllib.request.urlopen(search_source_url) as url:
+        search_source_data = url.read()
+        search_source_response = json.loads(search_source_data)
 
-        search_movie_results = None
+        search_source_results = None
 
-        if search_movie_response['results']:
-            search_movie_list = search_movie_response['results']
-            search_movie_results = process_results(search_movie_list)
+        if search_source_response['results']:
+            search_source_list = search_source_response['results']
+            search_source_results = process_results(search_source_list)
 
 
-    return search_movie_results
+    return search_source_results
