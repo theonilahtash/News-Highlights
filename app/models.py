0015a8203731fddb3d580fe11c107e6fcb64fed4
Theonilah Owali <theonilahtash@gmail.com>
2018-11-09 19:56:49 +0300

    add author and image url to the model article

--- app/models.py
+++ app/models.py
@@ -1,44 +1,29 @@
+
+class Article:
+    """ 
+    Article class to define Article Objects
+    """
+
+    def __init__(self, source, author, title, description, url, image_url, publish_time):
+
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image_url = image_url
+        self.publish_time = publish_time
+
 class Source:
-    '''
-    Source class to define News Objects
-    '''
+    """ 
+    Source class to define source objects
+    """
 
-    def __init__(self,id,name,description,poster,category,language,country):
-        self.id: time,
-        self.name: Time,
-        self.description: Breaking news and analysis from TIME.com. Politics, world news, photos, video, tech reviews, health, science and entertainment news,
-        self.poster: http://time.com,
-        self.category: general,
-        self.language: en,
-        self.country: us
+    def __init__(self, id, name, description, url, category, country):
 
-
-class Articles:
-
-    all_articles = []
-
-    def __init__(self,news_id,name,imageurl,article):
-        self.news_id = news_id
+        self.id = id
         self.name = name
-        self.imageurl = imageurl
-        self.articles = articles
-
-
-    def save_article(self):
-        Article.all_articles.append(self)
-
-
-    @classmethod
-    def clear_articles(cls):
-        Article.all_articles.clear()
-
-    @classmethod
-    def get_articles(cls,id):
-
-        response = []
-
-        for article in cls.all_article:
-            if article.news_id == id:
-                response.append(article)
-
-        return response
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
