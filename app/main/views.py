f4a0b9dc29424e0c9623b34a9cb2ea0176e42243
Theonilah Owali <theonilahtash@gmail.com>
2018-11-09 20:19:55 +0300

    made changes and rectified errors on my views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,63 +1,29 @@
 from flask import render_template,request,redirect,url_for
+
+from ..request import get_sources,get_article
+
 from . import main
-from ..request import get_source,get_source,search_source
-from .forms import ReviewForm
-from ..models import Review
+
 
 @main.route('/')
 def index():
+    """
+    Function that returns the index page and all its data
+    """
+    entertainment_news = get_sources('entertainment')
+    sport_news = get_sources('sports')
+    tech_news = get_sources('technology')
+    title = title
+    
+    return render_template('index.html', title=title, sports=sport_news, technology=tech_news, entertainment=entertainment_news)
 
-    '''
-    View root page function that returns the index page and its data
-    '''
 
-    # Getting news sources
-    Breaking_news = get_source('Breaking news')
-    Business_news = get_source('Business')
-    General_news = get_source('General')
-    title = 'Home - Welcome to The best News Review Website Online'
-
-    search_source = request.args.get('source_query')
-
-    if search_source:
-        return redirect(url_for('main.search',source_name=search_source))
-    else:
-        return render_template('index.html', name = name, Breaking = Breaking_news, Business = Business_news, General = General_news)
-# Views
-@main.route('/source/<int:id>')
-def source(id):
-
-    '''
-    View news page function that returns the source details page and its data
-    '''
-
-    Source = get_source(id)
-    name = f'{source.name}'
-    reviews = Review.get_reviews(source.id)
-    return render_template('news.html', name = name, source=source,reviews = reviews )
-
-@main.route('/search/<source_name>')
-def search(source_name):
-    '''
-    View function to display the search results
-    '''
-    source_name_list = source_name.split(" ")
-    source_name_format = "+".join(source_name_list)
-    searched_news = search_news(source_name_format)
-    name = f'search results for {source_name}'
-    return render_template('search.html',source = searched_source, name=name)
-
-@main.route('/source/review/news/<int:id>', methods = ['GET','POST'])
-def source_review(id):
-    form = ReviewForm()
-    source = get_source(id)
-
-    if form.validate_on_submit():
-        name = form.name.data
-        review = form.review.data
-        new_review = Review(news.id,name,source.url,review)
-        new_review.save_review()
-        return redirect(url_for('main.source',id = source.id ))
-
-    name = f'{source.name} review'
-    return render_template('new_review.html',name = name, review_form=form, source=source)
+@main.route('/source/<id>')
+def articles(id):
+    """
+    View articles
+    """
+    article = get_article(id)
+    print(article)
+    title = f'News{id}'
+    return render_template('news.html', title=title, article=article)
