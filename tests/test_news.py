67b17aa1befc78ddf2fadf9cc2088286d950ba48
Theonilah Owali <theonilahtash@gmail.com>
2018-11-13 22:00:02 +0300

    run my tests to ascertain that they pass

--- tests/test_news.py
+++ tests/test_news.py
@@ -1,10 +1,8 @@
 import unittest
-from .models import news
-
-News = news.News
+from app.models import Source,Articles
 
 
-class NewsTest(unittest.TestCase):
+class SourceTest(unittest.TestCase):
     """
     Test Class to test the behaviour of the News class
     """
@@ -13,33 +11,11 @@ class NewsTest(unittest.TestCase):
         """
         Set up method that will run before every test
         """
-        self.new_news = News(1234, 'Amazing news', 'Thrilling news', 'https://abcnews.go.com', 'Technology',
+        self.new_sources = Source(1234, 'Amazing news', 'Thrilling news', 'https://abcnews.go.com', 'Technology',
                              'UnitedStates', 'English')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_news, News))
-
-import unittest
-from app.models import News,Articles
-
-
-
-class NewsTest(unittest.TestCase):
-    def setUp(self):
-        self.new_news=News(90,'cool','new','damn','okay','nice','root')
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_news, News))
-
-    # def test_check_instance_variables(self):
-    #     self.assertEquals(self)
-    #     self.assertEquals(self.new_news.name,cool)
-    #     self.assertEquals(self.new_news.description,new)
-    #     self.assertEquals(self.new_news.url,damn)
-    #     self.assertEquals(self.new_news.category,okay)
-    #     self.assertEquals(self.new_news.language,nice)
-    #     self.assertEquals(self.new_news.country,root)    
-        
+        self.assertTrue(isinstance(self.new_sources, Source))
 
 class ArticlesTest(unittest.TestCase):
     def setUp(self):
@@ -48,18 +24,7 @@ class ArticlesTest(unittest.TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.new_articles, Articles)) 
 
-    # def test_check_instance_variables(self):
-    #     self.assertEquals(self.new_articles.author,nice)
-    #     self.assertEquals(self.new_articles.title,forr)
-    #     self.assertEquals(self.new_articles.description,what)
-    #     self.assertEquals(self.new_articles.url,ooh)
-    #     self.assertEquals(self.new_articles.urlToImage,woow)
-    #     self.assertEquals(self.new_articles.urlToImage,kiki)
-
-# 1.importing the news class that we created
-# 2.setup method enables our class to take in objects
-# 3.the isinstance functions chack if the objects created are instances of a class           
-©        
+      
 
 
 if __name__ == '__main__':
